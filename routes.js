a2897f04509d683c1a70dd0c4e925eb068804c0c
malikalrizky <110241221+malikalrizky@users.noreply.github.com>
2022-12-28 13:46:21 +0700

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,31 @@
+const handler = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: handler.addNewBook,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: handler.getAllBooks,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: handler.getBookById,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: handler.editBookById,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: handler.deleteBookById,
+  },
+];
+
+module.exports = routes;
